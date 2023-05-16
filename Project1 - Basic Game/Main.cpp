13807f011eba75ee65390641992d526a898df261
Len Farag <lfarag@riotgames.com>
2023-05-16 15:28:05 +1000

    Change Ashengine-Core to a library, and then made a new project for the basic game that uses the library.

--- Project1 - Basic Game/Main.cpp
+++ Project1 - Basic Game/Main.cpp
@@ -18,41 +18,41 @@ struct GameCode
 	Pixel ButtonClicked = Colour::WHITE;
 	Pixel ButtonCurrent = ButtonNormal;
 
-	void OnTick(Window& a_GameWindow, float a_DeltaTime)
+	void OnTick( Window& a_GameWindow, float a_DeltaTime )
 	{
-		if (Input::IsKeyDown(KeyCode::Right) && RectangleX < a_GameWindow.GetWidth() - RectangleWidth)
+		if ( Input::IsKeyDown( KeyCode::Right ) && RectangleX < a_GameWindow.GetWidth() - RectangleWidth )
 		{
 			RectangleX += RectangleMovementSpeed * a_DeltaTime;
 		}
 
-		if (Input::IsKeyDown(KeyCode::Left) && RectangleX > 0)
+		if ( Input::IsKeyDown( KeyCode::Left ) && RectangleX > 0 )
 		{
 			RectangleX -= RectangleMovementSpeed * a_DeltaTime;
 		}
 
-		if (Input::IsKeyDown(KeyCode::Up) && RectangleY > 0)
+		if ( Input::IsKeyDown( KeyCode::Up ) && RectangleY > 0 )
 		{
 			RectangleY -= RectangleMovementSpeed * a_DeltaTime;
 		}
 
-		if (Input::IsKeyDown(KeyCode::Down) && RectangleY < a_GameWindow.GetHeight() - RectangleHeight)
+		if ( Input::IsKeyDown( KeyCode::Down ) && RectangleY < a_GameWindow.GetHeight() - RectangleHeight )
 		{
 			RectangleY += RectangleMovementSpeed * a_DeltaTime;
 		}
 
 		// Make the rectangle that changes colour when you hover over it.
 		float MouseX, MouseY;
-		Input::GetMousePosition(MouseX, MouseY);
+		Input::GetMousePosition( MouseX, MouseY );
 
 		// If mouse is over the button
 		if (
-			(MouseX >= ButtonX) &&
-			(MouseX < ButtonX + ButtonWidth) &&
-			(MouseY >= ButtonY) &&
-			(MouseY < ButtonY + ButtonHeight))
+			( MouseX >= ButtonX ) &&
+			( MouseX < ButtonX + ButtonWidth ) &&
+			( MouseY >= ButtonY ) &&
+			( MouseY < ButtonY + ButtonHeight ) )
 		{
 			// If left clicking, change it's colour to the clicked colour;
-			if (Input::IsMouseDown(MouseCode::LeftMouse))
+			if ( Input::IsMouseDown( MouseCode::LeftMouse ) )
 			{
 				ButtonCurrent = ButtonClicked;
 			}
@@ -67,27 +67,27 @@ struct GameCode
 		}
 	}
 
-	void OnDraw(Window& a_GameWindow)
+	void OnDraw( Window& a_GameWindow )
 	{
-		a_GameWindow.SetRect(RectangleX, RectangleY, RectangleWidth, RectangleHeight, Colour::LIGHT_BLUE);
-		a_GameWindow.SetRect(ButtonX, ButtonY, ButtonWidth, ButtonHeight, ButtonCurrent);
+		a_GameWindow.SetRect( RectangleX, RectangleY, RectangleWidth, RectangleHeight, Colour::LIGHT_BLUE );
+		a_GameWindow.SetRect( ButtonX, ButtonY, ButtonWidth, ButtonHeight, ButtonCurrent );
 	}
 };
 
 int main()
 {
 	// Create a window to draw to.
-	Window GameWindow("New window!", 128, 128, 8, 8);
-	
+	Window GameWindow( "New window!", 128, 128, 8, 8 );
+
 	// Check if the new window creation was successful. If not, return error code.
-	if (!GameWindow.IsValid())
+	if ( !GameWindow.IsValid() )
 	{
 		return 1;
 	}
 
 	// Set the main window for this application to the one we just made.
 	const Window* GameWindowPointer = &GameWindow;
-	Window::SetMainWindow(GameWindowPointer);
+	Window::SetMainWindow( GameWindowPointer );
 
 	// Do things that need to happen once at the start, like setting up input.
 	Input::Init();
@@ -103,24 +103,24 @@ int main()
 	float DeltaTime = 0.0333;
 
 	// Run a game loop.
-	while (true)
+	while ( true )
 	{
 		// Tick things that need to happen each frame, like input.
 		Input::Tick();
 
 
 		// Clear the background ready for things to be drawn over the top of it.
-		GameWindow.SetBuffer(BackgroundPixel);
+		GameWindow.SetBuffer( BackgroundPixel );
 
 
 		// Run game code
 		// ...
-		SomeRandomGameCode.OnTick(GameWindow, DeltaTime);
+		SomeRandomGameCode.OnTick( GameWindow, DeltaTime );
 
 
 		// Run graphics code
 		// ...
-		SomeRandomGameCode.OnDraw(GameWindow);
+		SomeRandomGameCode.OnDraw( GameWindow );
 
 
 		// Draw to the window.
@@ -144,13 +144,13 @@ int main()
 		// The effect this has is pretty much the same as the VSync setting you see in some games, to try to lock
 		// the fps by padding out the time if the frame was too quick. It however doesn't work if the frame too LONGER
 		// than the target fps, since you'd have to pad it out negatively.
-		if (Milliseconds < 33.333)
+		if ( Milliseconds < 33.333 )
 		{
 			// Time left to pad it out to 30fps
 			int64_t PadTime = 33.3333 - Milliseconds;
 
 			// Let's sleep for that long to make the whole loop take 33.333 milliseconds
-			Sleep(PadTime);
+			Sleep( PadTime );
 		}
 	}
 }
